937e91bdd1d36b063a8973b6e4caaadae3654520
Anders <idris.maps@gmx.ch>
2019-04-03 18:41:12 +0200

    Variante avec Array.from

--- exemple.js
+++ exemple.js
@@ -8,17 +8,19 @@ const html = `<div>
   </ul>
 </div>`
 
+const lis = Array.from(cheerio('li', html))
+
+/*
 const div = cheerio('div', html)[0]
 const ul = R.path(['children', 1], div)
 const lis = R.prop('children', ul)
   .filter(R.propEq('type', 'tag'))
-
+*/
 const getId = R.path(['attribs', 'id'])
 const getText = R.path(['children', 0, 'data'])
-/*
+
 console.log(lis.map(li => ({
   id: getId(li),
   text: getText(li),
 })))
-*/
-console.log(div)
+
